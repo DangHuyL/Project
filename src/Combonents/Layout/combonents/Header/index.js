b6ca9148a7fdce06406c4bc3e8691563c910a16b
Dang Huy <nguyendanghuy481@gmail.com>
2022-08-23 10:37:37 +0700

    Xây dựng UI phần Header #1

--- src/Combonents/Layout/combonents/Header/index.js
+++ src/Combonents/Layout/combonents/Header/index.js
@@ -1,13 +1,38 @@
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Header.module.scss';
+import {
+    faCircleXmark,
+    faMagnifyingGlass,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
-
 function Header() {
     return (
         <div className={cx('wraper')}>
             <div className={cx('inner')}>
-                <h2>Header</h2>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok" />
+                </div>
+                <div className={cx('search')}>
+                    <input
+                        placeholder="Search accounts and videos"
+                        spellCheck="false"
+                    />
+                    <button className={cx('close')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon
+                        className={cx('loading')}
+                        icon={faSpinner}
+                    />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('active')}></div>
             </div>
         </div>
     );
